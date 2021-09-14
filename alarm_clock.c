9ec0180d5f26f5ba2f7490d49233d67c97e75c8d
Mattias Lindgren <mattias.swenne@gmail.com>
2021-09-14 10:43:05 +0200

    Added four time functions, no impl

--- alarm_clock.c
+++ alarm_clock.c
@@ -1,4 +1,16 @@
+/*
+Mattias Lindgren
+1995-05-05-6235
+mattls@kth.se
+*/
+
 #include <stdio.h>
+
+int get_current_time();
+int get_alarm_time();
+int update_time();
+
+int time_delay();
 
 //@MattiasLindgren278
 int main()
