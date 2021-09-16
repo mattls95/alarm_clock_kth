7ce68b6f7b4f59724a8cd01c4ce804cf4683e34f
Mattias Lindgren <mattias.swenne@gmail.com>
2021-09-16 22:59:03 +0200

    Made some changes

--- alarm_clock.c
+++ alarm_clock.c
@@ -1,46 +1,59 @@
-/*
-Mattias Lindgren
-1995-05-05-6235
-mattls@kth.se
-*/
-
 #include <stdio.h>
 
-int get_time();
-int update_time();
-void print_time(int);
-int check_time();
 
+void print_number(int);
+void update_time(int, int);
 
-int time_delay();
+int main() {
+    int present_time; 
+    int alarm_time;
 
-//@MattiasLindgren278
-int main()
-{
-    int present_time, time_for_alarm;
-    puts("What time is it?");
-    present_time = get_time();
-    puts("What time should the alarm be set to?");
-    time_for_alarm = get_time();
+    printf("What time is it? ");
+    scanf("%d", &present_time);
+
+    printf("What time should the alarm be set to? ");
+    scanf("%d", &alarm_time);
+
+    update_time(present_time, alarm_time);
+    printf("ALARM");
+
     return 0;
 }
 
-int get_time()
+void print_time(int present_hour, int present_min, int present_sec)
 {
-    int time;
-    scanf("%d", &time);
-    return time;
+    printf("%02d:%02d:%02d\n", present_hour, present_min, present_sec);
 }
 
-void check_print_time(int time)
+void update_time(int present_time, int alarm_time) 
 {
-    if(time < 10)
-    {
-        printf("0%d", time);
-    } else
-    {
-        printf("%d", time);
+    int present_hour = present_time/10000;
+    int present_min = (present_time/100) % 100;
+
+    int present_sec = (present_time % 100);
+    int alarm_hour = alarm_time/10000;
+
+    int alarm_min = (alarm_time/100) % 100;
+    int alarm_sec = (alarm_time % 100);
+
+    while (present_hour != alarm_hour) {
+            if (present_sec <= 60) {
+                present_sec++;
+                if (present_sec == 60) {
+                    present_sec = 0;
+                    present_min++;
+                }
+                if (present_min == 60) {
+                    present_min = 0;
+                    present_hour++;
+                }
+                if (present_hour == 24) {
+                    present_hour = 0;
+                }
+                //Ändra till do-while
+                
+                print_time(present_hour, present_min, present_sec);
+            }
+        
     }
 }
-
-
