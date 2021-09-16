c62b8cae4e4ae61ef50d8fd201c2eab65a0726b7
Mattias Lindgren <mattias.swenne@gmail.com>
2021-09-16 13:48:21 +0200

    Added get_time function

--- alarm_clock.c
+++ alarm_clock.c
@@ -6,8 +6,7 @@ mattls@kth.se
 
 #include <stdio.h>
 
-int get_current_time();
-int get_alarm_time();
+int get_time();
 int update_time();
 
 int time_delay();
@@ -15,5 +14,18 @@ int time_delay();
 //@MattiasLindgren278
 int main()
 {
+    int present_time, time_for_alarm;
+    puts("What time is it?");
+    present_time = get_time();
+    puts("What time should the alarm be set to?");
+    time_for_alarm = get_time();
     return 0;
+    
+}
+
+int get_time()
+{
+    int time;
+    scanf("%d", &time);
+    return time;
 }
