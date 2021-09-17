75de5f4f11a830ad585c3afadc7e3c5beccbbd15
Mattias Lindgren <mattias.swenne@gmail.com>
2021-09-17 12:39:04 +0200

    Added full functionality

--- test.c
+++ test.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 
-void print_number(int);
+void print_time(int, int, int);
 void update_time(int, int);
 
 int main() {
     int present_time; 
-    int alarm_time;
+    int time_for_alarm;
 
     printf("What time is it? ");
     scanf("%d", &present_time);
 
     printf("What time should the alarm be set to? ");
-    scanf("%d", &alarm_time);
+    scanf("%d", &time_for_alarm);
 
-    update_time(present_time, alarm_time);
+    update_time(present_time, time_for_alarm);
     printf("ALARM");
 
     return 0;
@@ -22,20 +22,20 @@ int main() {
 
 void print_time(int present_hour, int present_min, int present_sec)
 {
-    printf("%02d:%02d:%02d\n", present_hour, present_min, present_sec);
+    printf("%d:%d:%d\n", present_hour, present_min, present_sec);
 }
 
-void update_time(int present_time, int alarm_time) 
-{
+void update_time(int present_time, int time_for_alarm) {
     int present_hour = present_time/10000;
     int present_min = (present_time/100) % 100;
+    
     int present_sec = (present_time % 100);
+    int alarm_hour = time_for_alarm/10000;
 
-    int alarm_hour = alarm_time/10000;
-    int alarm_min = (alarm_time/100) % 100;
-    int alarm_sec = alarm_time % 100;
+    int alarm_min = (time_for_alarm/100) % 100;
+    int alarm_sec = (time_for_alarm % 100);
 
-    while (present_hour != alarm_hour) {
+    while (present_hour != alarm_hour && present_min != alarm_min) {
             if (present_sec <= 60) {
                 present_sec++;
                 if (present_sec == 60) {
@@ -49,30 +49,28 @@ void update_time(int present_time, int alarm_time)
                 if (present_hour == 24) {
                     present_hour = 0;
                 }
-                //Ändra till do-while
-                
                 print_time(present_hour, present_min, present_sec);
             }
         
     }
-
-    while(present_min != alarm_hour)
-    {
-       if(present_sec <= 60) {
+    while (present_min != alarm_min) {
+        if (present_hour == alarm_hour && present_min == alarm_min && present_sec == alarm_sec) {
+            printf("ALARM");
+        }
+        else {
+            if (present_sec <= 60) {
                 present_sec++;
 
                 if (present_sec == 60) {
                     present_sec = 0;
                     present_min++;
                 }
-        print_time(present_hour, present_min, present_sec);
+                print_time(present_hour, present_min, present_sec);
+            }
+        }
     }
-
-    }
- while(present_sec != alarm_sec)
-    {
+    while (present_sec != alarm_sec) {
+            print_time(present_hour, present_min, present_sec);
         present_sec++;
-        print_time(present_hour, present_min, present_sec);
     }
-
 }
