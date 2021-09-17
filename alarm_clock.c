f9f359975d44f10fcce3ed3495e13db427cd5c04
Mattias Lindgren <mattias.swenne@gmail.com>
2021-09-17 17:00:57 +0200

    Added a lot of stuff

--- alarm_clock.c
+++ alarm_clock.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
 void print_number(int);
 void update_time(int, int);
+void check_time(int, int ,int ,int ,int ,int);
 
-int main() {
+int main() 
+{
     int present_time; 
     int alarm_time;
 
@@ -15,15 +18,37 @@ int main() {
     scanf("%d", &alarm_time);
 
     update_time(present_time, alarm_time);
-    printf("ALARM");
-
     return 0;
 }
+
+/*
+The function prints time and adds a ':' between hours,minutes and seconds an a extra zero if needded
+Example: 0:0:0 becomes 00:00:00
+*/
 
 void print_time(int present_hour, int present_min, int present_sec)
 {
     printf("%02d:%02d:%02d\n", present_hour, present_min, present_sec);
 }
+
+/*
+Checks if condition for alarm has been meet and exits program
+*/
+
+void check_time(int present_hour, int present_min, int present_sec, int alarm_hour, int alarm_min, int alarm_sec)
+{
+    if (present_hour == alarm_hour && present_min == alarm_min && present_sec == alarm_sec)
+    {
+        printf("Alarm!");
+        exit(0);
+    }
+}
+
+/*
+The function updates the time in three steps first it compares the hours and updates hours, minutes and seconds if 
+nedded. It then compares minutes and updates minutes and seconds if needed, finally it updates seconds.
+After each check it calls the print_time function which then prints the new time.
+*/
 
 void update_time(int present_time, int alarm_time) 
 {
@@ -36,7 +61,7 @@ void update_time(int present_time, int alarm_time)
     int alarm_sec = alarm_time % 100;
 
     while (present_hour != alarm_hour) {
-            if (present_sec <= 60) {
+            if (present_sec < 60) {
                 present_sec++;
                 if (present_sec == 60) {
                     present_sec = 0;
@@ -49,22 +74,22 @@ void update_time(int present_time, int alarm_time)
                 if (present_hour == 24) {
                     present_hour = 0;
                 }
-                //Ändra till do-while
-                
+                check_time(present_hour, present_min, present_sec, alarm_hour, alarm_min, alarm_sec);
                 print_time(present_hour, present_min, present_sec);
             }
         
     }
 
-    while(present_min != alarm_hour)
+    while(present_min != alarm_min)
     {
-       if(present_sec <= 60) {
+       if(present_sec < 60) {
                 present_sec++;
 
                 if (present_sec == 60) {
                     present_sec = 0;
                     present_min++;
                 }
+        check_time(present_hour, present_min, present_sec, alarm_hour, alarm_min, alarm_sec);
         print_time(present_hour, present_min, present_sec);
     }
 
@@ -72,7 +97,10 @@ void update_time(int present_time, int alarm_time)
  while(present_sec != alarm_sec)
     {
         present_sec++;
+        check_time(present_hour, present_min, present_sec, alarm_hour, alarm_min, alarm_sec);
         print_time(present_hour, present_min, present_sec);
     }
 
 }
+
+
